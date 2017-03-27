1f60d0d333c65d1637123eefefbca8211042a435
Grigoriy Kramarenko <djbaldey@gmail.com>
2017-03-27 20:10:34 +1000

    Поддержка PY3 ещё не готова

--- setup.py
+++ setup.py
@@ -27,6 +27,5 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
     ],
 )
